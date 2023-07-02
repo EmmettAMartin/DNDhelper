a600f0414feb2299b9dbcbde029f11e75e5cfc07
Emmett <emmettmartin273@gmail.com>
2023-07-01 22:51:37 -0600

    Updated create_new_character method.

--- DNDHelper.py
+++ DNDHelper.py
@@ -4,6 +4,7 @@ import random as rand
 
 global name_text_box
 global health_text_box
+global character_count
 
 root = tk.Tk()
 
@@ -12,6 +13,8 @@ root.geometry("750x750")
 root.title("DNDhelper v1.0 - Not Resizable")
 
 root.resizable(False,False)
+
+title = tk.Label(text="DNDHelper")
 
 '''
 Features: Button that allows you to add a new character/NPC
@@ -48,21 +51,33 @@ current_die = tk.StringVar()
 
 current_die.set(dice_list[4])
 
+character_count = 1
 
-def create_new_text(x, y):
-    text = tk.Text(root, height=1, width=5)
-    text.place(x=x, y=y)
+def create_new_text(x, y, text):
+    text_box = tk.Text(root, height=1, width=5)
+    text_box.place(x=x, y=y)
+    text_box.insert(tk.END, str(text))
 
 def create_new_label(x, y, text):
     label = tk.Label(root, text=text, borderwidth=2, relief="solid")
     label.place(x=x, y=y)
 
-def create_new_character(x, y, name, health):
-    print("New Character Created")
+def create_new_character():
+    global character_count
+    if character_count > 15:
+        new_character_button["state"] = tk.DISABLED
     name = name_text_box.get("1.0", "end-1c")
     health = health_text_box.get("1.0","end-1c")
-    print(name)
-    print(health)
+    armour = armour_text_box.get("1.0","end-1c")
+    create_new_label(10, (40*character_count), text=name)
+    create_new_text(110, (40*character_count), text=health)
+    create_new_text(210, (40*character_count), text=armour)
+
+    for i in range(3):
+        #create_new_text()
+        pass
+
+    character_count += 1
 
 
 name_text_box_label = tk.Label(root, text="Name")
@@ -77,9 +92,14 @@ health_text_box_label.place(x=200, y=675)
 health_text_box = tk.Text(root, height=1, width=8)
 health_text_box.place(x=200,y=700)
 
+armour_text_box = tk.Text(root, height=1, width=8)
+armour_text_box.place(x=275,y=700)
+
+armour_text_box_label = tk.Label(root, text="Armour")
+armour_text_box_label.place(x=275, y=675)
 
 
-new_character_button = tk.Button(root, text="New Character", command=partial(create_new_character,5,5,"Orc1",5))
+new_character_button = tk.Button(root, text="New Character", command=create_new_character)
 new_character_button.place(x=10,y=700)
 
 
