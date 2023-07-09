9f7a3bd6a17491c58bc1d787f487b87d3f6dd418
Emmett <emmettmartin273@gmail.com>
2023-07-08 19:29:08 -0600

    Added a scrollable frame.

--- DNDHelper.py
+++ DNDHelper.py
@@ -53,25 +53,86 @@ current_die.set(dice_list[4])
 
 character_count = 1
 
+
+
+
+
+###############################################################################################
+# The Following code is not mine, and all credit goes to Sergeant_Ranger on github.           #
+###############################################################################################
+
+
+
+#MAIN FUNC
+def create_scrollable_frame(x, y, scrollable_frame_width, scrollable_frame_height):
+    container = tk.Frame(
+        root, height=scrollable_frame_height, width=scrollable_frame_width
+    )
+    canvas_container = tk.Frame(
+        container, height=scrollable_frame_height, width=scrollable_frame_width
+    )
+
+    result_canvas = tk.Canvas(canvas_container, width=scrollable_frame_width - 20)
+    scroller = tk.Scrollbar(
+        canvas_container, orient="vertical", command=result_canvas.yview
+    )
+
+    scrollable_frame = tk.Frame(result_canvas, width=scrollable_frame_width - 20)
+
+    scrollable_frame.bind(
+        "<Configure>",
+        lambda e: result_canvas.configure(scrollregion=result_canvas.bbox("all")),
+    )
+
+    result_canvas.create_window((0, 0), window=scrollable_frame, anchor="nw")
+    result_canvas.configure(yscrollcommand=scroller.set)
+    canvas_container.place(relheight=1, relwidth=1)
+    scroller.pack(side="right", fill="y")
+    result_canvas.pack(side="left", fill="both")
+
+    container.place(x=x, y=y)
+
+    def _on_mousewheel(event):
+        result_canvas.yview_scroll(int(-1 * (event.delta / 120)), "units")
+
+    scrollable_frame.bind_all("<MouseWheel>", _on_mousewheel)
+
+    return scrollable_frame
+
+#CREATE SCROLLABLE FRAME
+global scrollable_frame
+scrollable_frame = create_scrollable_frame(0, 0, 500, 650)
+
+#TREAT SCROLLABLE FRAME AS AN OTHERWISE NORMAL FRAME
+
+
+
+###############################################################################################
+# Thanks again to Sergeant_Ranger for letting me use this.                                    #
+###############################################################################################
+
+
+
+
+
 def create_new_text(x, y, text):
-    text_box = tk.Text(root, height=1, width=5)
+    text_box = tk.Text(scrollable_frame, height=1, width=5)
     text_box.place(x=x, y=y)
     text_box.insert(tk.END, str(text))
 
 def create_new_label(x, y, text):
-    label = tk.Label(root, text=text, borderwidth=2, relief="solid")
+    label = tk.Label(scrollable_frame, text=text, borderwidth=2, relief="solid")
     label.place(x=x, y=y)
 
 def create_new_character():
     global character_count
-    if character_count > 15:
-        new_character_button["state"] = tk.DISABLED
     name = name_text_box.get("1.0", "end-1c")
     health = health_text_box.get("1.0","end-1c")
     armour = armour_text_box.get("1.0","end-1c")
     create_new_label(10, (40*character_count), text=name)
     create_new_text(110, (40*character_count), text=health)
     create_new_text(210, (40*character_count), text=armour)
+    scrollable_frame.configure(height=40 * (character_count+1))
 
     for i in range(3):
         #create_new_text()
